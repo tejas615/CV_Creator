476bbdae9813332b0076bc47e97f2492f1942e65
tejas615 <tejas28032005@gmail.com>
2025-05-18 13:22:14 +0530

    Remove files

--- chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-
-
-router.post('/:pid',chatController.chat);
-router.get('/getchat/:pid', chatController.getchat);
-
-module.exports = router;
